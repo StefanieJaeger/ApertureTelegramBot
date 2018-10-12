fac1e289ea1ae7c53ba860f803d966f0a67f352b
Stefanie Jäger <stefijaeger@gmail.com>
2018-10-13 00:06:45 +0200

    Add first quote and get_quote method
    Co-authored-by: Jeremy Stucki <stucki.jeremy@gmail.com>

--- good_morning.py
+++ good_morning.py
@@ -1,5 +1,8 @@
 import telepot
 import os
+import yaml
+import random
+
 
 try:
     bot = telepot.Bot(os.environ['TELEGRAM_API_KEY'])
@@ -7,4 +10,14 @@ except telepot.exception.BadHTTPResponse:
     print('Received a bad HTTP response while getting connection to bot')
     exit(1)
 
-bot.sendMessage(os.environ['GROUP_ID'], "hay hay jem jem")
+
+def get_quote():
+    with open('good_morning_quotes.yml', encoding='utf8') as file:
+        try:
+            quotes = yaml.load(file.read())['quotes']
+            return random.choice(quotes)
+        except:
+            print('Unable to read quotes')
+
+
+bot.sendMessage(os.environ['GROUP_ID'], get_quote())
