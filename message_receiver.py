23b6b532f7dd44e7de60695de2193897a11b97c6
Stefanie Jäger <stefijaeger@gmail.com>
2018-10-14 21:38:24 +0200

    Add first quotes and handle leaving chat member

--- message_receiver.py
+++ message_receiver.py
@@ -4,17 +4,21 @@ import yaml
 import random
 import time
 
-
 try:
     bot = telepot.Bot(os.environ['TELEGRAM_API_KEY'])
 except telepot.exception.BadHTTPResponse:
     print('Received a bad HTTP response while getting connection to bot')
     exit(1)
 
-
 with open('greeting_quotes.yml', encoding='utf8') as file:
     try:
-        quotes = yaml.load(file.read())['quotes']
+        greeting_quotes = yaml.load(file.read())['quotes']
+    except:
+        print('Unable to read quotes')
+
+with open('goodbye_quotes.yml', encoding='utf8') as file:
+    try:
+        goodbye_quotes = yaml.load(file.read())['quotes']
     except:
         print('Unable to read quotes')
 
@@ -22,7 +26,10 @@ with open('greeting_quotes.yml', encoding='utf8') as file:
 # see https://core.telegram.org/bots/api
 def handle(message):
     if 'new_chat_members' in message:
-        return bot.sendMessage(message['chat']['id'], random.choice(quotes))
+        return bot.sendMessage(message['chat']['id'], random.choice(greeting_quotes))
+
+    if 'left_chat_member' in message:
+        return bot.sendMessage(message['chat']['id'], random.choice(goodbye_quotes))
 
 
 bot.message_loop(handle)
